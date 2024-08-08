3e678102ad1986fbf5cb464cfed7abbaebaf9097
Augusto <augustotsantos@outlook.com>
2024-08-08 14:29:37 -0300

    feat(TM12)!: add constructors

--- api/src/main/java/selaz/api/entity/association/Association.java
+++ api/src/main/java/selaz/api/entity/association/Association.java
@@ -1,11 +1,15 @@
 package selaz.api.entity.association;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import selaz.api.entity.user.User;
 import selaz.api.entity.task.Task;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "ass_association")
 public class Association {
